4995f6b592cd0917fd31d7f98a122df42fa0da9e
Abdurahim Khudoyberdiev <abdurahim.khudoyberdiev@gmail.com>
2022-07-31 02:24:10 +0500

    Add events

--- DataTables.Blazor.Demo/wwwroot/js/demo.js
+++ DataTables.Blazor.Demo/wwwroot/js/demo.js
@@ -2,5 +2,9 @@
     logSummary: (td, cellData, rowData, row, col) => {
         console.log(`(Row ${(row + 1)}, Column ${(col + 1)}) = ${cellData}`);
     },
-    renderDate: data => new Date(data).toLocaleString('en-GB')
+    renderDate: data => new Date(data).toLocaleString('en-GB'),
+    scrollToBottom: (elementId) => {
+        var element = document.getElementById(elementId);
+        element.scrollTop = element.scrollHeight;
+    }
 };
