4dff89ca564c393c0ae3f8cc08d2a38aa196add8
Abdurahim Khudoyberdiev <abdurahim.khudoyberdiev@gmail.com>
2022-07-31 06:26:42 +0500

    Add RowClick  EventArgs

---
+++ DataTables.Blazor/Events/RowClickEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text; 
+
+namespace DataTables.Blazor.Events
+{
+    public class RowClickEventArgs : EventArgs
+    {
+        public string? Id { get; set; }
+        public dynamic Data { get; set; } 
+    }
+}
