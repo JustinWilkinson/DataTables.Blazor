5f8bf13ec93bdb3b2e3cf9b734d4ad90732d2310
Justin Wilkinson <j.wilkinson@codat.io>
2024-01-10 18:07:48 +0000

    Update license, fix compiler warnings

--- DataTables.Blazor/Extensions/ReflectionExtensions.cs
+++ DataTables.Blazor/Extensions/ReflectionExtensions.cs
@@ -14,10 +14,11 @@ internal static class ReflectionExtensions
     /// <param name="property">Property to obtain setter of.</param>
     /// <param name="type">Type of instance.</param>
     /// <returns>A delegate that invokes the property setter.</returns>
-    public static Func<object, object> GetGetter(this PropertyInfo property, Type type) => CreateGetDelegate(property.GetMethod, type);
+    public static Func<object, object?> GetGetter(this PropertyInfo property, Type type) 
+        => CreateGetDelegate(property.GetMethod!, type);
 
     #region Helpers
-    private static readonly MethodInfo GenericGetHelper = typeof(ReflectionExtensions).GetMethod(nameof(GetDelegateHelper), BindingFlags.Static | BindingFlags.NonPublic);
+    private static readonly MethodInfo GenericGetHelper = typeof(ReflectionExtensions).GetMethod(nameof(GetDelegateHelper), BindingFlags.Static | BindingFlags.NonPublic)!;
 
     /// <summary>
     /// Helper method that returns a getter for a property using the object class.
@@ -25,13 +26,13 @@ internal static class ReflectionExtensions
     /// <param name="method">Method Info to pass</param>
     /// <param name="type">Type of instance</param>
     /// <returns></returns>
-    private static Func<object, object> CreateGetDelegate(MethodInfo method, Type type)
+    private static Func<object, object?> CreateGetDelegate(MethodInfo method, Type type)
     {
         // Supply the type arguments to the generic helper.
         var genericMethod = GenericGetHelper.MakeGenericMethod(type, method.ReturnType);
 
         // Cast the result to the right kind of delegate and return it. The null argument is because it's a static method
-        return (Func<object, object>)genericMethod.Invoke(null, new[] { method });
+        return (Func<object, object?>)genericMethod.Invoke(null, new[] { method })!;
     }
 
     /// <summary>
@@ -41,13 +42,13 @@ internal static class ReflectionExtensions
     /// <typeparam name="TParam">Parameter type</typeparam>
     /// <param name="method">Get Method</param>
     /// <returns>A weakly typed delegate that calls a strongly type one.</returns>
-    private static Func<object, object> GetDelegateHelper<TTarget, TParam>(MethodInfo method) where TTarget : class
+    private static Func<object, object?> GetDelegateHelper<TTarget, TParam>(MethodInfo method) where TTarget : class
     {
         // Convert the slow MethodInfo into a fast, strongly typed, open delegate
         var func = CreateTypedFunction<TTarget, TParam>(method);
 
         // Now create a more weakly typed delegate which will call the strongly typed one
-        return target => (TParam)Convert.ChangeType(func((TTarget)Convert.ChangeType(target, typeof(TTarget))), typeof(TParam));
+        return target => (TParam?)Convert.ChangeType(func((TTarget)Convert.ChangeType(target, typeof(TTarget))), typeof(TParam));
     }
 
     /// <summary>
